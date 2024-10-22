ca29582886f46a02d91e156802053d894021497d
William Norland <william@luwisoft.com>
2024-10-22 10:46:51 +0200

    Timestamps

--- analysis.py
+++ analysis.py
@@ -2,13 +2,20 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Create chart with 985x812 pixels
-plt.figure(figsize=(985/100, 812/100), dpi=100)
+# plt.figure(figsize=(985/100, 812/100), dpi=100)
 
 # Read data from csv file
 data = pd.read_csv('985x812_394x180.csv')
 
-# Get all points that have speed 0
-data = data[data['dx'] == 0]
+# Stretch the x axis
+data['t'] = data['t'] * 100
 
+# Plot x position as a function of time
+plt.plot(data['t'], data['x'])
 
+# Add labels
+plt.xlabel('t')
+plt.ylabel('x')
 
+# Show chart
+plt.show()
